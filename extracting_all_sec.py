1404581493c9c34715fbdc8d8dd95a41f0dfe23a
Kedar Kenjalkar <130866781+kedargh@users.noreply.github.com>
2024-10-14 01:31:59 +0530

    Update extracting_all_sec.py
    Extracts ticker data using yfinance API. 
    Creates individual dataframes for each stock and then writes to the Supabase

--- extracting_all_sec.py
+++ extracting_all_sec.py
@@ -1,60 +1,38 @@
-import requests
-import pandas as pd
+import yfinance as yf
 from supabase import create_client, Client
-import os
-import csv 
+import pandas as pd
 
-ALPHAVANTAGE_API_KEY = '409HR7EQNP9WGW3A'
 supabase_Url = 'https://xsujstzsbguabmmfdoww.supabase.co'
 supabase_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhzdWpzdHpzYmd1YWJtbWZkb3d3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgyODg5MzEsImV4cCI6MjA0Mzg2NDkzMX0.yl_1pRMfQtCcXASs9KVt2snmHkiamGXML24VnLeXKaI'
 supabase: Client = create_client(supabase_Url, supabase_key)
 
-def get_stock_data_daily(symbol):
-    base_url = 'https://www.alphavantage.co/query'
-    params = {
-        'function': 'TIME_SERIES_DAILY',  
-        'symbol': symbol, 
-        'apikey': ALPHAVANTAGE_API_KEY,
-        'outputsize': 'full'  
-    }
-    response = requests.get(base_url, params=params)
-    data = response.json()
-    return data
+
+df = pd.read_csv('EQUITY_L.csv' , skiprows=1, header=None)
+df.columns = ['SYMBOL', 'NAME OF COMPANY', 'SERIES', 'DATE OF LISTING', 'PAID UP VALUE', 'MARKET LOT', 'ISIN NUMBER', 'FACE VALUE']
+tickers = df['SYMBOL'].tolist()
+csv_file_path = "equity_data.csv"
 
 
-nse_symbols = ['INFY']  # Have to write the code to fetch all the different symbols
+print(tickers)
+all_data = pd.DataFrame()
 
-data = [['Date' , 'Opening Value' , 'High' , 'Low' , 'SecurityID']]
+with open(csv_file_path, 'w') as f:
+    pass 
 
-
-for symbol in nse_symbols:
-    print(f"Fetching data for {symbol}...")
-    stock_data = get_stock_data_daily(symbol)
-    
-    time_series = stock_data['Time Series (Daily)']
-
-    for key,value in time_series.items():
-        ts_opening_value = value['1. open']
-        ts_date = key
-        ts_high = value['2. high']
-        ts_low = value['3. low']
-        data.append([ts_date, ts_opening_value , ts_high , ts_low , symbol])
-        print(f"Date : {ts_date} , Opening Value : {ts_opening_value} , High : {ts_high} , Low : {ts_low} , Symbol : {symbol}")
-
-    with open('stock_data_daily.csv', mode='w', newline='') as file:
-        writer = csv.writer(file)    
-        writer.writerows(data)
-    print("CSV file created successfully!")
-
-    data = []
-#--------------------------------------------------------------------------------------------------------------------
-    df = pd.read_csv('stock_data_daily.csv')
-    data = df.to_dict(orient='records')
-    response = supabase.table('EQUITY_PRICES').insert(data).execute()
-
-    if response.data:
-        print(f"All rows successfully entered")
+for i,ticker_symbol in enumerate(tickers):
+    data = yf.Ticker(ticker_symbol)
+    historical_data = data.history(period="max").reset_index()
+    historical_data['ticker'] = ticker_symbol
+    if i == 0:
+        historical_data.to_csv(csv_file_path, index=False, mode='w', header=True)
     else:
-        print(f"Failed to insert row . Error: {response.json()}")
+        historical_data.to_csv(csv_file_path, index=False, mode='a', header=False)
 
-    print(f"Inserted Data for {symbol}")
+    print(f"Data for stock {ticker_symbol} successfully added to CSV")
+
+print("CSV CONSTRUCTED SUCCESSFULLY !!!!")
+
+with open(csv_file_path, 'rb') as f:
+    res = supabase.storage.from_('equity_data_bucket').upload("equity_data.csv", f , {"upsert" : "true"})
+    print("File uploaded to Supabase storage")
+
