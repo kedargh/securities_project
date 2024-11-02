cc952ccfb3c1917539bc88927b1fbae41354d20f
kedargh <kedarkenjalkar@gmail.com>
2024-11-02 13:01:52 +0530

    Bulk upload and data cleansing done

--- src/extracting_all_sec.py
+++ src/extracting_all_sec.py
@@ -34,7 +34,7 @@ def execute_any_query(query , SUPABASE_URL , SUPABASE_DB , SUPABASE_USER , SUPAB
     finally:
         if CURSOR:
             CURSOR.close()
-
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 def parse_xml_config_and_create_table(xml_file_path):
     supabase= create_supabase_client(xml_file_path)
     tree = ET.parse(xml_file_path)
@@ -68,8 +68,7 @@ def parse_xml_config_and_create_table(xml_file_path):
         create_table_sql += f"    {field_name} {field_type},\n"
     create_table_sql = create_table_sql.rstrip(',\n') + "\n);"  
     print(create_table_sql)
-
-#---------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------
     with open(f"/home/kedar/securities_project/securities_project/sql_files/{table_name}.sql", "w") as sql_file:
         sql_file.write(create_table_sql)
         print("WRITTEN QUERY TO FILE !!!")
@@ -80,37 +79,33 @@ def parse_xml_config_and_create_table(xml_file_path):
         file_name = os.path.basename(upload_file.name)
         res = supabase.storage.from_('equity_data_bucket').upload(file = upload_file,path=f"create_table_files/{file_name}", file_options={"upsert" : "true"})
         print("File uploaded to Supabase storage")
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-#-------------------------------------------------------------------------------------------
-
-def read_local_csv_to_supabase_storage():
-    df = pd.read_csv('/home/kedar/securities_project/securities_project/data/EQUITY_L.csv' , skiprows=1, header=None)
+def daily_series_data():
+    df = pd.read_csv('/home/kedar/securities_project/securities_project/data/equity_list.csv' , skiprows=1, header=None)
     df.columns = ['SYMBOL', 'NAME OF COMPANY', 'SERIES', 'DATE OF LISTING', 'PAID UP VALUE', 'MARKET LOT', 'ISIN NUMBER', 'FACE VALUE']
     tickers = df['SYMBOL'].tolist()
     csv_file_path = "/home/kedar/securities_project/securities_project/data/equity_data.csv"
-
-
-    print(tickers)
-    all_data = pd.DataFrame()
+    supabase = create_supabase_client(xml_file_path_1)
     no_data_tickers = []
-    with open(csv_file_path, 'w') as f:
-        pass 
+    print(tickers)
 
-    for i,ticker_symbol in enumerate(tickers):
-        data = yf.Ticker(ticker_symbol)
+    i = 0
+    for ticker in tickers:
+        data = yf.Ticker(ticker)
         historical_data = data.history(period="max").reset_index()
         if historical_data.empty:
-            no_data_tickers.append(ticker_symbol)
-            print(f"No data available for stock {ticker_symbol}")
-            continue
-        
-        historical_data['ticker'] = ticker_symbol
-        if i == 0:
-            historical_data.to_csv(csv_file_path, index=False, mode='w', header=True)
+            no_data_tickers.append(ticker)
         else:
-            historical_data.to_csv(csv_file_path, index=False, mode='a', header=False)
+            selected_data = historical_data[["Date","Open","High","Low","Close","Volume","Dividends","Stock Splits"]]
+            selected_data['ticker'] = ticker
+            if i == 0:
+                selected_data.to_csv(csv_file_path, index=False, mode='w', header=True)
+            else:
+                selected_data.to_csv(csv_file_path, index=False, mode='a', header=False)
+            i += 1
 
-        print(f"Data for stock {ticker_symbol} successfully added to CSV")
+            print(f"Data for stock {ticker} successfully added to CSV")
 
     print("CSV CONSTRUCTED SUCCESSFULLY !!!!")
     print(no_data_tickers)
@@ -118,16 +113,7 @@ def read_local_csv_to_supabase_storage():
     with open(csv_file_path, 'rb') as f:
         res = supabase.storage.from_('equity_data_bucket').upload("/home/kedar/securities_project/data/equity_data.csv", f , {"upsert" : "true"})
         print("File uploaded to Supabase storage")
-
-def ensure_index(df):
-    if isinstance(df.index, pd.RangeIndex):
-        print("The DataFrame is using a default numeric index.")
-    else:
-        print("The DataFrame has a custom index.")
-        # If it has a custom index, reset it to a default numeric index
-        df.reset_index(drop=True, inplace=True)  # Drop the old index and reset
-
-    return df
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 def bulk_upload(xml_file_path):
     tree = ET.parse(xml_file_path)
@@ -140,23 +126,18 @@ def bulk_upload(xml_file_path):
     #print(table_name)
     supabase: Client = create_client(db_url,api_key)
     df = pd.read_csv(file_path)
-    print(df.head())
+    #print(df.head())
     cleaned_data = [{k.strip(): v for k, v in record.items()} for record in df.to_dict(orient="records")]
-    print(cleaned_data)
+    print(cleaned_data[1])
     print("CONVERTED TO RECORDS")
-    response = supabase.table(table_name).insert(cleaned_data).execute()
-    # if response.error:
-    #     print(response.error)
-    # else:
-    #     print("BULK UPLOAD ACCOMPLISHED",response.data)
-
-
+    try:
+        response = supabase.table(table_name).insert(cleaned_data).execute()
+    except Exception as e:
+        print("Error during upload:", e)
 
 if __name__ == "__main__":
-    #upload_file_to_supabase_storage('/home/kedar/securities_project/sql_files/EQUITY_INFO.sql', 'equity_data_bucket')
-    parse_xml_config_and_create_table(xml_file_path_1)
+    # parse_xml_config_and_create_table(xml_file_path_1)
     parse_xml_config_and_create_table(xml_file_path_2)
-    bulk_upload(xml_file_path_1)
+    daily_series_data()
+    #bulk_upload(xml_file_path_1)
     bulk_upload(xml_file_path_2)
-    #read_local_csv_to_supabase_storage()
-    #read_local_csv_to_supabase_storage()
