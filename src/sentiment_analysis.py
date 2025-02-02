e297f77f176680b9701a3d0983b9030c904ba3fb
Kedar Kenjalkar <130866781+kedargh@users.noreply.github.com>
2025-02-02 19:19:03 +0530

    Update sentiment_analysis.py
    Created new agents. Added groupchat object

--- src/sentiment_analysis.py
+++ src/sentiment_analysis.py
@@ -1,6 +1,13 @@
 from gnews import GNews
 import csv
 from autogen_agentchat.agents import AssistantAgent
+from autogen_agentchat.base import TaskResult
+from autogen_agentchat.conditions import ExternalTermination, TextMentionTermination
+from autogen_agentchat.teams import RoundRobinGroupChat
+from autogen_agentchat.agents import UserProxyAgent
+from pydantic import BaseModel
+from typing import List, Dict
+from autogen_agentchat.ui import Console
 from autogen_agentchat.messages import TextMessage
 from autogen_agentchat.ui import Console
 from autogen_core import CancellationToken
@@ -9,18 +16,22 @@ import openai
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 import torch
 from news_fetcher import fetch_news_for_ticker
+#################MESSAGE FORMATS FOR AGENTS#############################################
+class NewsResponse(BaseModel):
+    stock: str
+    headlines: List[str]
 
+class SentimentResponse(BaseModel):
+    stock: str
+    results: List[Dict[str, str]]
 
-# def create_training_csv_file(headlines):
-#     file_name = "training_dataset.csv"
-#     with open(file_name, mode="w", newline="") as file:
-#         writer = csv.writer(file)
-#         writer.writerow(["sentences"])
-#         for headline in headlines:
-#             writer.writerow([headline])
-
-
-async def headlines_fetcher(stock : str , number_of_news: int)->list:         #####TOOL 1####
+############################################################################################################################
+model_client = OpenAIChatCompletionClient(
+    model="gpt-4o-mini",
+    api_key="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",
+)
+################################################################################################################################
+async def headlines_fetcher(stock : str , number_of_news: int)->NewsResponse:         #####TOOL 1####
     """Fetch the list of news for the given stock ticker"""
     google_news = GNews(
     language='en',
@@ -38,54 +49,79 @@ async def headlines_fetcher(stock : str , number_of_news: int)->list:         ##
     for headline in headlines:
         headline = headline.split('-')[0].strip()
     
-    return(headlines)
+    #return headlines
+    return NewsResponse(stock=stock, headlines=headlines)
 
 
-async def sentiment_classifier(text: str) -> str:   #####TOOL 2#####
-    """Derive sentiment for the given text."""
-    model_path = "yiyanghkust/finbert-tone"
-    tokenizer = AutoTokenizer.from_pretrained(model_path)
-    model = AutoModelForSequenceClassification.from_pretrained(model_path)
+model_path = "yiyanghkust/finbert-tone"
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = AutoModelForSequenceClassification.from_pretrained(model_path)
+model.eval()
 
-    inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
 
-    with torch.no_grad():
-        outputs = model(**inputs)
-        predictions = torch.softmax(outputs.logits, dim=1)
-        label_index = torch.argmax(predictions).item()
+async def sentiment_classifier_loop(stock: str, list_of_news: List[str]) -> SentimentResponse:
+    """Classify sentiment for multiple news articles asynchronously and return structured response."""
+    async def sentiment_classifier(text: str) -> Dict[str,str]:
+        """Single news classification"""
+        inputs = tokenizer(news, return_tensors="pt", truncation=True, padding=True)
 
-    labels = ["Neutral", "Positive", "Negative"]
-    sentiment = labels[label_index]
-    print(sentiment)
-    return sentiment
+        with torch.no_grad():
+            outputs = model(**inputs)
+            predictions = torch.softmax(outputs.logits, dim=1)
+            label_index = torch.argmax(predictions).item()
+        labels = ["Neutral", "Positive", "Negative"]
+        return {text: labels[label_index]}
 
-async def sentiment_classifier_loop(list_of_news: list) -> list:
-    """Run this classifier loop on a list of news for the stock"""
-    list_of_classified_sentiments = []
-    for news in list_of_news:
-        list_of_classified_sentiments.append(sentiment_classifier(news))
-    print(sentiment_classifier)
-    return list_of_classified_sentiments
+    tasks = [sentiment_classifier(news) for news in list_of_news]
+    results = await asyncio.gather(*tasks)
 
-model_client = OpenAIChatCompletionClient(
-    model="gpt-4o-mini",
-    api_key="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",
-)
+    return SentimentResponse(stock=stock , results=results)
 
-agent = AssistantAgent(
-    name="assistant",
+# async def run_agent():
+#     user_message = "Fetch 10 headlines for TCS using the headlines_fetcher tool, then perform sentiment analysis on each headline using the sentiment_classifier_loop tool."
+#     response = await agent.on_messages(
+#     messages=[TextMessage(content=user_message, source="user")],
+#     cancellation_token=CancellationToken()
+#     )
+#     print(f"Assistant response: {response}")
+
+# async def assistant_run() -> None:
+#     user_message = (
+#         "Perform sentiment analysis for the following statement"
+#     )
+#     response = await agent.on_messages(
+#         [TextMessage(content=user_message, source="user")],
+#         cancellation_token=CancellationToken(),
+#     )
+#     print(response.inner_messages)
+#     print(response.chat_message)
+#####################AGENTS###################################################################
+news_agent = AssistantAgent(
+    name="NewsFetcherAgent",
+    description="Fetches financial news for a given stock ticker.",
     model_client=model_client,
-    tools=[headlines_fetcher,sentiment_classifier_loop,sentiment_classifier], #headlines_fetcher , , sentiment_classifier_loop
+    tools=[headlines_fetcher],
+    system_message="You are a helpful assistant. Use tools when needed.",
+
+)
+sentiment_classifier_agent = AssistantAgent(
+    name="SentimentClassifierAgent",
+    description="Classifies sentiment for a list of news articles.",
+    model_client=model_client,
+    tools=[sentiment_classifier_loop], #headlines_fetcher , , sentiment_classifier_loop
     system_message="You are a helpful assistant. Use tools when needed.",
 )
 
-async def run_agent():
-    user_message = "Perform sentiment analysis for TCS. The (headlines_fetcher) function returns the list of a headline of the company ticker (TCS). For the list of headline fetched by the (headlines_fetcher) function perform sentiment analysis using the (sentiment_classifier) function"#"Perform sentiment analysis for TCS. The (headlines_fetcher) function returns the list of a headline of the company ticker (TCS). For the list of headline fetched by the (headlines_fetcher) function perform sentiment analysis using the (sentiment_classifier) function"
-    response = await agent.on_messages(
-        messages=[TextMessage(content=user_message, source="user")],
-        cancellation_token=CancellationToken()
-    )
-    print(f"Assistant response: {response}")
+prompt_agent = UserProxyAgent(
+    name="PromptAgent",
+    description='A question asking agent.',
 
-import asyncio
-asyncio.run(run_agent())  
+)
+
+################################################################################################################
+async def main():
+    team = RoundRobinGroupChat([supervision_agent, news_agent , sentiment_classifier_agent], termination_condition=text_termination)
+    result = await team.run(task="Perform sentiment analysis for Infosys.")
+    print(result)
+
+asyncio.run(main())
