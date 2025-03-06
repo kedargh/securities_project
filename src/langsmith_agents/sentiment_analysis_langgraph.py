953d8791cec599cb2205452413099f68bcb19927
Kedar Kenjalkar <130866781+kedargh@users.noreply.github.com>
2025-03-07 00:41:44 +0530

    partially working graph

--- src/langsmith_agents/sentiment_analysis_langgraph.py
+++ src/langsmith_agents/sentiment_analysis_langgraph.py
@@ -1,71 +1,57 @@
-from typing import Annotated
-from langgraph.graph import StateGraph
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
 from langgraph.graph import Graph
-from langgraph.graph.message import add_messages
-from typing_extensions import TypedDict
-import yfinance as yf
-from gnews import GNews
+from rich.console import Console
 from pydantic import BaseModel
 from typing import List, Dict
+from gnews import GNews
+import yfinance as yf
+import torch
 
-class NewsState(BaseModel):
-    stock: str
-    headlines: List[str]
-
-class SentimentState(BaseModel):
-    stock: str
-    results: List[Dict[str, str]]
-
-class StockState(BaseModel):  #####PYDANTIC FOR STOCK ATTRIBUTES######
-    stock: str
-    attributes: Dict
-
-class PromptInputState:
-    prompt: str
-
-class DecisionState:
-    action_to_perform: str
+initial_state = {"prompt":None ,"stock": None, "ticker": None , "number_of_news":None,"headlines": None , "sentiments": None , "company_data":None}
 
 
-graph_builder = Graph()
+##############NOTB A NODE###########################
 
-def input_prompt() -> PromptInputState : 
-    prompt_1 = str(input("What type of computation do you want to perform and on which stock? \n"))
-    return PromptInputState(prompt=prompt_1) 
+model_path = "yiyanghkust/finbert-tone"
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = AutoModelForSequenceClassification.from_pretrained(model_path)
+model.eval()
+def sentiment_classifier_single_news(news: str) -> str:
+        """Single news classification"""
+        inputs = tokenizer([news], return_tensors="pt", truncation=True, padding=True)
 
-#####CONDITIONAL NODE#####
-def prompt_analysis(prompt_received:PromptInputState)->DecisionState:
-    if("sentiment analysis" in prompt_received.prompt):
-        return('sentiment_analysis')
-    elif("market research" in prompt_received.prompt):
-        return('market_research')
+        with torch.no_grad():
+            outputs = model(**inputs)
+            predictions = torch.softmax(outputs.logits, dim=1)
+            label_index = torch.argmax(predictions).item()
+        labels = ["Neutral", "Positive", "Negative"]
+        return labels[label_index]
+
+
+
+###############################################################
+def take_input(initial_state):
+    prompt = str(input("Enter the operation you want to perform : "))
+    initial_state["prompt"] = prompt
+    return initial_state
+
+def input_prompt_output(initial_state):
+    if "market research" in initial_state["prompt"].lower() or "market study" in initial_state["prompt"].lower():
+        return "market_research"
+    elif "sentiment analysis" in initial_state["prompt"].lower() or "sentiment" in initial_state["prompt"].lower():
+        return "sentiment_classifier"
     
 
-def fetch_company_details(stockstate_obj : StockState)-> StockState:
-    stock_name = stockstate_obj.stock
-    data = yf.Ticker(stock_name)
-    dict_for_analyst_price_targets = data.analyst_price_targets or {}
-    dict_for_ticker_info = data.info or {}
-    stock_details = {**dict_for_analyst_price_targets, **dict_for_ticker_info}
-
-    return StockState(stock=stock_name,attributes=stock_details)
-
-
-def headlines_fetcher(newsstate_obj : NewsState)->NewsState:         #####TOOL 1####
-    """Fetch the list of news for the given stock ticker"""
-   
-    stock = newsstate_obj["stock"] 
-    number_of_news = newsstate_obj["number_of_news"]
-
+def market_research(inital_state):
     google_news = GNews(
     language='en',
     country='US',
     period='1d',
     start_date=None,
     end_date=None,
-    max_results=number_of_news,)
+    max_results=50,)
     ####GOOGLE NEWS OBJECT#####
-    stock = newsstate_obj.stock
+    stock=initial_state["stock"]
     news = google_news.get_news(f"{stock}")
     headlines = []
     for single_news in news:
@@ -73,22 +59,41 @@ def headlines_fetcher(newsstate_obj : NewsState)->NewsState:         #####TOOL 1
 
     for headline in headlines:
         headline = headline.split('-')[0].strip()
-    
-    #return headlines
-    return NewsState(stock=stock, headlines=headlines)
+
+    inital_state["headlines"] = headlines
+    return inital_state
 
 
 
-if __name__ == "__main__":
+def sentiment_classifier(initial_state):
+    headlines = initial_state["headlines"]
+    classified_sentiment_output = []
+    for headline in headlines:
+        classified_sentiment_output.append(sentiment_classifier_single_news(headlines))
 
-    graph_builder.add_node("company_details" , fetch_company_details)
-    graph_builder.add_node("stock_news" , headlines_fetcher)
-    graph_builder.set_entry_point("stock_news")
-    graph_builder.set_finish_point("stock_news")
-    graph = graph_builder.compile()
+    initial_state["sentiments"] = classified_sentiment_output
+    return initial_state
 
-    input_state = {"stock":"INFY" , "number_of_news":6 ,  "headlines":[]}
-    print("INPUT STATE ADDED")
-    output_state = graph.invoke(input_state)
 
-    print("Stock Details : " , output_state)
+
+def end_node(initial_state):
+    print(f"Your processed input is: {initial_state}")
+    return initial_state
+
+workflow = Graph()
+
+workflow.add_node("start" , take_input)
+workflow.add_node("conditional_node", input_prompt_output)
+workflow.add_node("market_research", market_research)
+workflow.add_node("sentiment_classifier", sentiment_classifier)
+workflow.add_node("end" , end_node)
+
+workflow.add_conditional_edges(
+    "start", input_prompt_output,
+    {"market_research":"market_research" , "sentiment_classifier":"sentiment_classifier"}
+)   
+workflow.set_entry_point("start")
+compiled_workflow = workflow.compile()
+final_state = compiled_workflow.invoke(initial_state)
+
+print("Final State:", final_state)
